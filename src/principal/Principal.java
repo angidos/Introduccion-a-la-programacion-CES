1d42767a861c938c57861bc3025fe93688dee957
Angela <angidos97@gmail.com.uy>
2026-05-11 00:52:41 -0300

    first commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,8 @@
+package principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        // primer linea de código!
+        System.out.println("Hola mundo!");
+    }
+}
